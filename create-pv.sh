834680f33f9e3a8c4687d2163e74710db4686709
Jim Barlow <jim@jimbarlow.com>
2019-12-16 13:17:24 -0500

    initial commit

--- create-pv.sh
+++ create-pv.sh
@@ -5,16 +5,15 @@
 # a "for i in * ; do oc create -f $i ; done" loop
 
 export NFS_SERVER="helper.ocp3.example.com"
-export volsize="1Gi"
-export NUM_VOLUMES=2
+export volsize="6Gi"
+export NUM_VOLUMES=3
 export YAML_FILES_DIR="/root"
 
 mkdir -p $YAML_FILES_DIR
 
-for pvnum in {1..$NUM_VOLUMES}; do mkdir -p /exports/user-vols/${volsize}-${volume} ; done
+for pvnum in $(seq 1 $NUM_VOLUMES); do mkdir -p /exports/user-vols/${volsize}-${volume} ; done
 
-for pvnum in {1..$NUM_VOLUMES} ; do
-echo "/exports/user-vols/${volsize}${pvnum} *(rw,root_squash)" >> /etc/exports.d/openshift-uservols.exports ; 
+for pvnum in  $(seq 1 $NUM_VOLUMES) ; do echo "/exports/user-vols/${volsize}${pvnum} *(rw,root_squash)" >> /etc/exports.d/openshift-uservols.exports ; 
 done
 chown -R nfsnobody.nfsnobody  /exports
 chmod -R 777 /exports
@@ -22,12 +21,12 @@ chmod -R 777 /exports
 systemctl restart nfs-server
 
 
-for volume in 1..$NUM_VOLUMES ; do cat << ENDJASON > /$YAML_FILES_DIR/${volsize}-${volume}.yml ; done 
+for volume in $(seq 1 $NUM_VOLUMES);  do cat << ENDJASON > /$YAML_FILES_DIR/${volsize}-${volume}.yml ; done 
 {
   "apiVersion": "v1",
   "kind": "PersistentVolume",
   "metadata": {
-    "name": "${volsize}-${volume}"
+    "name": "pv-${volsize}-${volume}"
   },
   "spec": {
     "capacity": {
@@ -42,5 +41,4 @@ for volume in 1..$NUM_VOLUMES ; do cat << ENDJASON > /$YAML_FILES_DIR/${volsize}
   }
 }
 ENDJASON
-
 
