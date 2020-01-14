d48501ae370e988413f87b4c1be8633b1f0fd0b4
root <root@helper.ocp4.example.com>
2020-01-14 10:53:54 -0500

    enhancing for ocp4

--- fix_firewall_before_deploy_cluster_run.sh
+++ fix_firewall_before_deploy_cluster_run.sh
@@ -2,4 +2,10 @@
 # run this on helper node so that nfs is available to persist during install runs
 firewall-cmd --zone=public --permanent --add-service=nfs
 firewall-cmd --zone=public --permanent --add-service=dns
+firewall-cmd --zone=public --permanent --add-service=dhcp
+firewall-cmd --zone=public --permanent --add-port=6443/tcp
+firewall-cmd --zone=public --permanent --add-port=22623/tcp
+firewall-cmd --zone=public --permanent --add-port=2380/tcp
+firewall-cmd --zone=public --permanent --add-service=http
+firewall-cmd --zone=public --permanent --add-service=https
 firewall-cmd --reload
